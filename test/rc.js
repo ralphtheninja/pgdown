080a3d6c409e348af87ac1e08402c8df11bbaa90
Lars-Magnus Skog <ralphtheninja@riseup.net>
2016-02-24 02:31:36 +0100

    use docker + use default settings for user and database

--- test/rc.js
+++ test/rc.js
@@ -1,3 +1,3 @@
 module.exports = require('rc')('pgdown', {
-  uri: 'postgres://pgdown_testuser:@localhost:5432/pgdown_testdb/pgdown_test_table'
+  uri: 'postgres://postgres:@localhost:5432/postgres/pgdown_test_table'
 })
